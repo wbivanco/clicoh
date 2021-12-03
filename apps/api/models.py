ab5f7fc81a8e13f7196604a9bbdc78d09a31401b
Walter Bivanco <walterbivanco@gmail.com>
2021-12-03 09:36:22 -0300

    Se crearon los modelos y las migraciones de Product, Order, OrderDetail

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+    class Meta:
+        verbose_name = "Productos"
+
+
+class Order(models.Model):
+    datetime = models.DateTimeField()
+
+    class Meta:
+        verbose_name = "Ordenes"
+
+
+class OrderDetail(models.Model):
+    cuantity = models.IntegerField()
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}'.format(self.order, self.product)
+
+    class Meta:
+        verbose_name = "Detalle Ordenes"
